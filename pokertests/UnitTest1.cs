5f9bfe6f3c17f342411119ae98aca8d87ec447e5
MarkusBernhardt2 <markus.bernhardt@live.de>
2017-02-28 10:35:32 +0100

    test

---
+++ pokertests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nancy.Simple;
+
+namespace pokertests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BetRequestReturnsValue()
+        {
+
+            Assert.AreEqual(5,  PokerPlayer.BetRequest(null));
+
+
+        }
+
+        [TestMethod]
+        public void CanConvertJson()
+        {
+            RootObject test = PokerPlayer.CreateClass("");
+
+        }
+    }
+}
--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Nancy.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Nancy.Simple
 {
@@ -8,13 +9,43 @@ namespace Nancy.Simple
 
 		public static int BetRequest(JObject gameState)
 		{
-            return 5;
+            RootObject test = CreateClass(gameState.ToString());
+
+            var us = FindUs(test);
+
+            if (us == null)
+            {
+                return 5;
+            }
+            
+
+            return 10;
 		}
+
+        public static Player FindUs(RootObject root)
+        {
+            foreach (var element in root.players)
+            {
+                if (element.name == "still mono noobs")
+                    return element;
+            }
+
+            return null;
+        }
 
 		public static void ShowDown(JObject gameState)
 		{
 			//TODO: Use this method to showdown
 		}
+
+        public static RootObject CreateClass(string gameState)
+        {
+
+            JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+            RootObject routes_list =
+                   (RootObject)json_serializer.DeserializeObject(gameState);
+            return routes_list;
+        }
 	}
 }
 
